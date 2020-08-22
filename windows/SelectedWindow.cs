b1e7af0ee09935fb1a3d5ba55139b1352405a13b
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-22 13:58:56 +0100

    Window selecting and moving

---
+++ windows/SelectedWindow.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace MultiWindows.windows
+{
+	public class SelectedWindow : Object
+	{
+		public BaseWindow Window;
+		public Vector2 HeldOffsetFromCenter;
+
+		public SelectedWindow(BaseWindow window, Vector2 heldOffsetFromCenter)
+		{
+			Window = window;
+			HeldOffsetFromCenter = heldOffsetFromCenter;
+		}
+	}
+}
---
+++ windows/Windows.cs
@@ -0,0 +1,51 @@
+using Godot;
+
+namespace MultiWindows.windows
+{
+	public class Windows : Node2D
+	{
+		public static SelectedWindow SelectedWindow;
+
+		public override void _Ready()
+		{
+			foreach (BaseWindow window in GetChildren())
+			{
+				window.Connect("input_event", this, nameof(OnWindowInputEvent), new Godot.Collections.Array() { window });
+			}
+		}
+
+		public override void _PhysicsProcess(float delta)
+		{
+			if (SelectedWindow is SelectedWindow)
+			{
+				SelectedWindow.Window.Position = GetLocalMousePosition() - SelectedWindow.HeldOffsetFromCenter;
+			}
+		}
+
+		private void OnWindowInputEvent(object viewport, object @event, int shape_idx, BaseWindow window)
+		{
+			if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.ButtonIndex == (int)ButtonList.Left && eventMouseButton.Pressed && SelectedWindow?.Window != window)
+			{
+				SelectWindow(window);
+			}
+		}
+
+		public override void _Input(InputEvent @event)
+		{
+			if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.ButtonIndex == (int)ButtonList.Left && !eventMouseButton.Pressed)
+			{
+				DeselectWindow();
+			}
+		}
+
+		private void SelectWindow(BaseWindow window)
+		{
+			SelectedWindow = new SelectedWindow(window, window.GetLocalMousePosition());
+		}
+
+		private void DeselectWindow()
+		{
+			SelectedWindow = null;
+		}
+	}
+}
